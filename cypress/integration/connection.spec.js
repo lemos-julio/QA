fd03c7dc0efac3b0b6460c3efe132effc6a2df07
lemos-julio <julio2012lemos@gmail.com>
2022-04-14 00:23:45 -0300

    Update Tests

--- cypress/integration/connection.spec.js
+++ cypress/integration/connection.spec.js
@@ -1,21 +1,38 @@
         
-        describe('Create a Connection', ()=>{
-            it('', ()=>{
-                cy.get('span:contains(Create connection)').click()
-                //cy.contains('Create connection').should('be.visible').click()
-                cy.get('input[id="connection-builder-name-input"]').clear().type('Connection-Test-SheetGO')
-                cy.contains('Single file').should('be.visible').click()
-                cy.contains('Select file').should('be.visible').click()
-                cy.contains('Source.xlsx').click()
-                cy.wait(10000)
-                cy.contains('Next').click()
-                cy.get('span:contains(Send data to a single spreadsheet)').should('be.visible').click()
-                cy.contains('Finish and save').should('be.visible').click()
-                cy.wait(10000)})
-            
-            })
+describe('Create a Connection', ()=>{
+    it('Criando Connection', ()=>{
+        cy.visit('https://app.sheetgo.com/login').wait(10000)
+        cy.get('button[data-onboarding-id="new-workflow"]').click()
+        cy.get('span:contains(Create connection)').click()
+    })
 
-            cy.get('button[aria-label="More"]').click()
-            cy.wait(10000)
-            cy.contains('Delete').click({force:true})
-            cy.contains('Delete connection').click()
+
+    it('Set a name for Connection',()=>{
+        cy.get('input[id="connection-builder-name-input"]')
+            .clear()
+            .type('Connection-Test-SheetGO')
+    })        
+    
+    it('Select a Destiny for Connection',() =>{
+        cy.contains('Single file')
+            .should('be.visible')
+            .click()
+        cy.contains('Select file')
+            .should('be.visible')
+            .click()
+        cy.contains('Source.xlsx').click()
+            .wait(10000)
+
+        cy.contains('Next').click()
+        cy.get('span:contains(Send data to a single spreadsheet)').should('be.visible').click()
+        cy.contains('Finish and save').should('be.visible').click()
+        cy.wait(10000)
+    })
+
+    it('Delete the Connection', ()=>{
+        //cy.get('path[d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"]').click({force:true})
+        cy.get('button[aria-label="More"]').click()
+        cy.contains('Delete').click({force:true})
+        cy.contains('Delete connection').click()
+    })
+})
--- cypress/integration/workflow.spec.js
+++ cypress/integration/workflow.spec.js
@@ -1,20 +1,58 @@
 /// <reference types="cypress" />
 describe('Testing Plataform SheetGO',()=>{
     beforeEach(() => {
-        cy.visit('https://app.sheetgo.com/login')
-        .wait(10000)
+        cy.visit('https://app.sheetgo.com/login').wait(10000)
     })
 
-    it.skip('Create WorkFLow', () => {
+    it('Create WorkFlow', () => {
         cy.get('button[data-onboarding-id="new-workflow"]').click()
-        //cy.get('button[title="My workspace"]').click()
-       //cy.get('li[id="workflow-menu-settings-mobile"]').should('be.visible')
+        cy.get('span:contains(Create connection)').click()
+        cy.contains('Single file')
+            .should('be.visible')
+            .click()
+        cy.contains('Select file')
+            .should('be.visible')
+            .click()
+        cy.contains('Source.xlsx').click()
+        cy.wait(10000)
+        cy.contains('Next').click()
+        cy.get('span:contains(Send data to a single spreadsheet)')
+            .should('be.visible')
+            .click()
+        cy.contains('Finish and save')
+            .should('be.visible')
+            .click()
+    
     })
 
-    it('Rename Workflow', ()=> {
-       
-        cy.get('div[data-onboarding-id="select-workflow"]').trigger('mouseenter', 'center').wait(1000)
-        //cy.get('button[aria-owns="workflow-settings-menu"]').click()
+    it('Rename WorkFlow',()=>{
+        cy.get('button[title="Open settings"]')
+            .invoke('show')
+            .click({force:true})
+        cy.get('li[id="workflow-menu-settings-mobile"]')
+            .contains('Edit information')
+            .click()
+        cy.get('input[id="workflow-settings-input"]')
+            .clear()
+            .type('WorkFlow SheetGO QA')
+        cy.get('input[id="workflow-settings-other"]')
+            .clear()
+            .type('QA')
+        cy.get('span:contains(Save)').click()
+
+
     })
+   
+   
+    it('Delete Workflow', ()=> {
+        cy.get('button[title="Open settings"]')
+            .invoke('show')
+            .click({force:true})
+        cy.contains('Delete workflow').click()
+        cy.get('button[aria-label="Delete workflow"]').click()
+    })
+
+
+    
 
 })
